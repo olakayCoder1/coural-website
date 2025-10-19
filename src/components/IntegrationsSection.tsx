135b1538cdae77f63936bc2fd11547ff38fb3a00
olakayCoder1 <programmerolakay@gmail.com>
2025-10-19 19:31:44 +0100

    `Removed commented out text in IntegrationsSection component`

--- src/components/IntegrationsSection.tsx
+++ src/components/IntegrationsSection.tsx
@@ -132,7 +132,7 @@ export default function IntegrationsSection(
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, delay: 0.1 }}
           >
-            Connect Everything. <br className="hidden sm:block" />
+            {/* Connect Everything. <br className="hidden sm:block" /> */}
             <span className="sm:hidden">Connect Everything. </span>
             Understand Everyone
           </motion.h2>
